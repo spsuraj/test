d3b81c0d9914f13d64841c51394021e96bd7fb0c
Suraj <spsuraj555@gmail.com>
2019-11-21 19:49:14 +0530

    New files Signed-off-by: Suraj <spsuraj555@gmail.com>

---
+++ http.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var url = require('url');
+var server = http.createServer(function (req, res) {
+res.writeHead(200, ('Content-Type', 'text/html'));
+var q = url.parse(req.url, true).query;
+var txt = q.year + " " + q.month;
+ res.end(txt);
+});
+server.listen(8082);
