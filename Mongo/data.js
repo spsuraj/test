8ddc2260b0e2f88a30fd41fa6909fd81c2616b5c
Suraj <spsuraj555@gmail.com>
2019-11-27 19:53:21 +0530

    Update

---
+++ Mongo/data.js
@@ -0,0 +1,39 @@
+const data = [
+    {
+        "id" : 1,
+        "firstName" : "Suraj",
+        "lastName" : "Sp",
+        "email" : "spsuraj555@gmail.com",
+        "site" : "http://ryachenkie.com"
+    },
+    {
+        "id" : 2,
+        "firstName" : "Whut",
+        "lastName" : "SWhutp",
+        "email" : "@gmail.com",
+        "site" : "http://ryachenkie.com"
+    },
+    {
+        "id" : 3,
+        "firstName" : "345",
+        "lastName" : "12",
+        "email" : "555@gmail.com",
+        "site" : "http://ryachenkie.com"
+    },
+    {
+        "id" : 4,
+        "firstName" : "dd",
+        "lastName" : "cc",
+        "email" : "suraj@gmail.com",
+        "site" : "http://ryachenkie.com"
+    },
+    {
+        "id" : 5,
+        "firstName" : "bb",
+        "lastName" : "aa",
+        "email" : "1234@gmail.com",
+        "site" : "http://ryachenkie.com"
+    },
+];
+
+module.exports = data;
